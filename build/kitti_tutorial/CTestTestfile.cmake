093632a9d60c4dfdebe727cc7e6ae0efa0b4579a
root <root@localhost.localdomain>
2021-11-29 10:31:47 +0800

    kitti_tutorial

---
+++ build/kitti_tutorial/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dikstra/catkin_ws/src/kitti_tutorial
+# Build directory: /home/dikstra/catkin_ws/build/kitti_tutorial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
